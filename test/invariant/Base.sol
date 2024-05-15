b32fbabc791951494fd2b1842da785e476665ee6
José Garção <garcao.random@gmail.com>
2024-05-15 13:45:37 +0100

    add configure and users to echidna base contract

--- test/invariant/Base.sol
+++ test/invariant/Base.sol
@@ -6,46 +6,59 @@ import "../../lib/properties/contracts/util/PropertiesHelper.sol";
 import {User} from "./utils/User.sol";
 import {HexitToken} from "../../src/HexitToken.sol";
 import {HexOneToken} from "../../src/HexOneToken.sol";
-import {HexOnePriceFeed} from "../../src/HexOnePriceFeed.sol";
+import {HexOnePriceFeedMock} from "../mocks/HexOnePriceFeedMock.sol";
 import {HexOneBootstrap} from "../../src/HexOneBootstrap.sol";
 import {HexOneVault} from "../../src/HexOneVault.sol";
 import {HexOnePoolManager} from "../../src/HexOnePoolManager.sol";
 import {ERC20Mock} from "../mocks/ERC20Mock.sol";
+import {StableMock} from "../mocks/StableMock.sol";
 
 contract Base is PropertiesAsserts {
-    uint256 public constant NUMBER_OF_USERS = 10;
-    uint256 public constant INITIAL_TOKEN_MINT = 10; // @todo set
-    uint256 public constant INITIAL_HEX_MINT = 10; // @todo set
+    uint256 constant NUMBER_OF_USERS = 10;
+    uint256 constant NUMBER_OF_STAKE_TOKENS = 2;
+    uint256 constant NUMBER_OF_SACRIFICE_TOKENS = 4;
+    uint256 constant INITIAL_TOKEN_MINT = 1_000_000_000 ether;
+    uint256 constant INITIAL_HEX_MINT = 1_000_000_000e8;
+    uint256 constant INITIAL_STABLE_MINT = 1_000_000_000e6;
 
-    User[] public users;
-    HexitToken public hexit;
-    HexOneToken public hex1;
-    HexOnePriceFeed public feed;
-    HexOneBootstrap public bootstrap;
-    HexOneVault public vault;
-    HexOnePoolManager public manager;
-    // HexMockToken public hx;
-    ERC20Mock public hx; // @todo fix
-    ERC20Mock public dai;
-    ERC20Mock public wpls;
-    ERC20Mock public plsx;
-    ERC20Mock public hex1dai;
+    User[] users;
+    HexitToken hexit;
+    HexOneToken hex1;
+    HexOnePriceFeedMock feed;
+    HexOneBootstrap bootstrap;
+    HexOneVault vault;
+    HexOnePoolManager manager;
+    // HexMockToken hx;
+    ERC20Mock hx; // @todo fix
+    ERC20Mock dai;
+    ERC20Mock wpls;
+    ERC20Mock plsx;
+    ERC20Mock hex1Dai;
+    ERC20Mock hexitHex1;
+    StableMock usdc;
+    StableMock usdt;
 
-    address[] public stakeTokens;
-    address[] public sacrificeTokens;
+    address[] stakeTokens;
+    address[] sacrificeTokens;
 
     constructor() {
-        users = new User[](NUMBER_OF_USERS);
+        _deploy();
+        _configure();
+        _users();
+    }
 
+    function _deploy() internal {
         hexit = new HexitToken();
-        dai = new ERC20Mock("DAI", "DAI");
+        feed = new HexOnePriceFeedMock();
+
         hx = new ERC20Mock("HEX", "HEX");
+        dai = new ERC20Mock("DAI", "DAI");
         wpls = new ERC20Mock("WPLS", "WPLS");
         plsx = new ERC20Mock("PLSX", "PLSX");
+        usdc = new StableMock("USDC", "USDC");
+        usdt = new StableMock("USDT", "USDT");
 
-        feed = new HexOnePriceFeed(address(hexit), 500);
-
-        sacrificeTokens = new address[](4);
+        sacrificeTokens = new address[](NUMBER_OF_SACRIFICE_TOKENS);
         sacrificeTokens[0] = address(hx);
         sacrificeTokens[1] = address(dai);
         sacrificeTokens[2] = address(wpls);
@@ -57,5 +70,94 @@ contract Base is PropertiesAsserts {
         vault = new HexOneVault(address(feed), address(bootstrap));
         hex1 = HexOneToken(vault.hex1());
         manager = new HexOnePoolManager(address(hexit));
+    }
+
+    function _configure() internal {
+        // configure hexit
+        hexit.initFeed(address(feed));
+        hexit.initBootstrap(address(bootstrap));
+        hexit.initManager(address(manager));
+
+        // configure feed
+        // HEX/DAI
+        address[] memory hexDaiPath = new address[](3);
+        hexDaiPath[0] = address(hx);
+        hexDaiPath[1] = address(wpls);
+        hexDaiPath[2] = address(dai);
+        feed.addPath(hexDaiPath);
+
+        // HEX/USDC
+        address[] memory hexUsdcPath = new address[](3);
+        hexUsdcPath[0] = address(hx);
+        hexUsdcPath[1] = address(wpls);
+        hexUsdcPath[2] = address(usdc);
+        feed.addPath(hexUsdcPath);
+
+        // HEX/USDT
+        address[] memory hexUsdtPath = new address[](3);
+        hexUsdtPath[0] = address(hx);
+        hexUsdtPath[1] = address(wpls);
+        hexUsdtPath[2] = address(usdt);
+        feed.addPath(hexUsdtPath);
+
+        // WPLS/DAI path
+        address[] memory wplsDaiPath = new address[](2);
+        wplsDaiPath[0] = address(wpls);
+        wplsDaiPath[1] = address(dai);
+        feed.addPath(wplsDaiPath);
+
+        // PLSX/DAI path
+        address[] memory plsxDaiPath = new address[](2);
+        plsxDaiPath[0] = address(plsx);
+        plsxDaiPath[1] = address(dai);
+        feed.addPath(plsxDaiPath);
+
+        // configure bootstrap
+        bootstrap.initVault(address(vault));
+
+        // create HEX1/DAI and HEXIT/HEX1 pairs in pulsex v2
+        hex1Dai = ERC20Mock(address(uint160(uint256(keccak256(abi.encode(hex1, dai))))));
+        hexitHex1 = ERC20Mock(address(uint160(uint256(keccak256(abi.encode(hexit, hex1))))));
+
+        // deploy HEX1/DAI and HEXIT pools
+        stakeTokens = new address[](NUMBER_OF_STAKE_TOKENS);
+        stakeTokens[0] = address(hex1Dai);
+        stakeTokens[1] = address(hexit);
+
+        uint256[] memory rewardsPerToken = new uint256[](NUMBER_OF_STAKE_TOKENS);
+        rewardsPerToken[0] = 420e18;
+        rewardsPerToken[1] = 69e18;
+
+        manager.createPools(stakeTokens, rewardsPerToken);
+    }
+
+    function _users() internal {
+        users = new User[](NUMBER_OF_USERS);
+
+        for (uint256 i; i < NUMBER_OF_USERS; ++i) {
+            User user = new User();
+            users.push(user);
+
+            dai.mint(address(user), INITIAL_TOKEN_MINT);
+            usdc.mint(address(user), INITIAL_STABLE_MINT);
+            usdt.mint(address(user), INITIAL_STABLE_MINT);
+
+            hx.mint(address(user), INITIAL_HEX_MINT);
+            wpls.mint(address(user), INITIAL_TOKEN_MINT);
+            plsx.mint(address(user), INITIAL_TOKEN_MINT);
+
+            user.approveERC20(dai, address(bootstrap));
+            user.approveERC20(hx, address(bootstrap));
+            user.approveERC20(wpls, address(bootstrap));
+            user.approveERC20(plsx, address(bootstrap));
+
+            // user.approveERC20(hex1, address(vault));
+        }
+
+        // @todo setPrices
+    }
+
+    function foo() public {
+        assert(true);
     }
 }
--- test/mocks/HexOnePriceFeedMock.sol
+++ test/mocks/HexOnePriceFeedMock.sol
@@ -8,6 +8,12 @@ contract HexOnePriceFeedMock {
 
     constructor() {}
 
+    function addPath(address[] memory _path) external {
+        address tokenIn = _path[0];
+        address tokenOut = _path[_path.length - 1];
+        paths[tokenIn][tokenOut] = _path;
+    }
+
     function setRate(address tokenIn, address tokenOut, uint256 r) external {
         rates[tokenIn][tokenOut] = r;
     }
@@ -26,7 +32,7 @@ contract HexOnePriceFeedMock {
 
     function update(address, address) external {}
 
-    function consult(address tokenIn, uint256 amountIn, address tokenOut) external view returns (uint256 amountOut) {
+    function quote(address tokenIn, uint256 amountIn, address tokenOut) external view returns (uint256 amountOut) {
         return amountIn * rates[tokenIn][tokenOut] / 1e18;
     }
 }
