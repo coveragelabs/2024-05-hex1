e459614804ff79455ed85b4c4df6f7eae2eeaa73
0xnexusflip <0xnexusflip@gmail.com>
2024-05-27 17:23:18 +0100

    fix paths

--- test/invariant/HexOneProperties.sol
+++ test/invariant/HexOneProperties.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import "../Base.sol";
+import "../invariant/Base.sol";
 
 import {hevm} from "../../../lib/properties/contracts/util/Hevm.sol";
 
